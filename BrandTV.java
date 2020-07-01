a0ffab89e00d1a822a7cd604c3157af901ff8e41
mjp002 <67648603+mjp002@users.noreply.github.com>
2020-07-01 12:16:47 +0900

    Create BrandTV.java

---
+++ BrandTV.java
@@ -0,0 +1,18 @@
+package JC;
+
+public class BrandTV extends TV {
+    
+    String brand;
+
+    public BrandTV(int intValue, String strValue) {
+        super.channel = intValue;
+        this.brand = strValue;
+    }
+
+    public static void main(String args[]) {
+        BrandTV objTV = new BrandTV(11, "삼성TV");
+        objTV.channelUp();
+        objTV.channelDown();
+        System.out.println(objTV.brand + " 현재 채널은 " + objTV.channel + "번입니다.");
+    }
+}
